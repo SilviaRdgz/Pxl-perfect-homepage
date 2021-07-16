a1c3a9ce3339a0f771a6aea9a5429d8c2c63545c
silvia rodriguez <silrodcar@gmail.com>
2021-07-16 16:12:59 +0200

    v.2 - assignment completed. added hover effects, transitions and applied feedback

--- JS/hamburger-script.js
+++ JS/hamburger-script.js
@@ -1,10 +1,7 @@
 function showMenu() {
     var element = document.getElementById("mainMenu");
     element.classList.add("showMenu");
-    console.log(element)
  }
-
-
 
  function closeMenu() {
     var element = document.getElementById("mainMenu");
